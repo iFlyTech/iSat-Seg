fed9712c71ccf3b4b3182b40ba83d3821c7ed6f0
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-06-26 21:12:03 +0000

    updated Japanese translation

--- src/create_spacenet_masks.py
+++ src/create_spacenet_masks.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python2
-# -*- coding: utf-
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  5 16:56:31 2017
+
+@author: avanetten
+"""
+
+from __future__ import print_function
+import os
+import sys
+import time
+import argparse
+import pandas as pd
+
+# ad
