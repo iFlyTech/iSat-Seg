d70bfa8bde75dbfb59bd3ac0353d1d27c315121e
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-08-05 19:42:55 +0000

    fixed function call

--- src/create_spacenet_masks.py
+++ src/create_spacenet_masks.py
@@ -31,4 +31,7 @@ def create_masks(path_data, buffer_meters=2, n_bands=3,
     Write 8bit images and masks to file.
     Return a dataframe of file locations with the following columns:
         ['name', 'im_file', 'im_vis_file', 'mask_file', 'mask_vis_file']
-    We record locations of im_vis_file an
+    We record locations of im_vis_file and mask_vis_file in case im_file
+      or mask_file is not 8-bit or has n_channels != [1,3]
+    if using 8band data, the RGB-PanSharpen_8bit should already exist, so
+ 
