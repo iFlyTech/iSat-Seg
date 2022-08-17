269686e3cb85c8a3e590a5941f6e67d6fdd1b7cd
iFlyTech <166435299+iFlyTech@users.noreply.github.com>
2022-08-17 11:22:37 +0000

    3.11.2

--- src/create_spacenet_masks.py
+++ src/create_spacenet_masks.py
@@ -34,4 +34,10 @@ def create_masks(path_data, buffer_meters=2, n_bands=3,
     We record locations of im_vis_file and mask_vis_file in case im_file
       or mask_file is not 8-bit or has n_channels != [1,3]
     if using 8band data, the RGB-PanSharpen_8bit should already exist, so
- 
+        3band should be run prior to 8band
+    '''
+    
+    t0 = time.time()
+    # set paths
+    #path_apls = os.path.dirname(path_apls_src)
+    #path_data = os.path.
